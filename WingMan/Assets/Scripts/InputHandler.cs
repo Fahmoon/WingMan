71dcdc7a4de81d222f058ccf9a09f1fdd187a522
Fahmy Hady <fahmy.hady@maugames.com>
2020-02-09 14:51:17 +0200

    InputHandler

---
+++ WingMan/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    static Vector2 touchDelta;
+    static Touch myTouch;
+
+
+    public static Touch GetTouch()
+    {
+        return myTouch;
+    }
+    public static Vector2 GetTouchDelta()
+    {
+        return touchDelta;
+    }
+    private void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            myTouch = Input.GetTouch(0);
+            touchDelta = myTouch.deltaPosition;
+        }
+        else
+        {
+            touchDelta = Vector2.zero;
+        }
+    }
+}
